d2307a4f00899f11043b30fe0739631cde96009f
Christopher Welborn <cj@welbornprod.com>
2016-12-21 22:08:20 -0600

    Add package names to auto-complete script, like apt-get does for 'source'.

--- apttool_completion.sh
+++ apttool_completion.sh
@@ -8,8 +8,15 @@ _apttool()
     # Get current word for completion.
     cur="${COMP_WORDS[COMP_CWORD]}"
 
-    if [ "$COMP_CWORD" -ge 1 ]; then
-        COMPREPLY=( $( compgen -fW '-? --examples -h --help -v --version -c= --containsfile= -C --nocolor -n --names -q --quiet -i --install -d --delete -p --purge -C --nocolor -q --quiet -e --executables -f --files -S --suggests -C --nocolor -q --quiet -s --short -P --dependencies -R --reversedeps -C --nocolor -I --INSTALLED -N --NOTINSTALLED -q --quiet -s --short -H --history -C --nocolor -q --quiet -l --locate -L --LOCATE -C --nocolor -q --quiet -s --short -u --update -C --nocolor -q --quiet -V --VERSION -C --nocolor -a --all -q --quiet -s --short -a --all -C --nocolor -I --INSTALLED -N --NOTINSTALLED -D --dev -n --names -q --quiet -r --reverse -s --short -x --ignorecase ' -- "$cur") )
+    if ((COMP_CWORD)); then
+        if [[ "$cur" == -* ]]; then
+            COMPREPLY=( $( compgen -fW '-? --examples -h --help -v --version -c= --containsfile= -C --nocolor -n --names -q --quiet -i --install -d --delete -p --purge -C --nocolor -q --quiet -e --executables -f --files -S --suggests -C --nocolor -q --quiet -s --short -P --dependencies -R --reversedeps -C --nocolor -I --INSTALLED -N --NOTINSTALLED -q --quiet -s --short -H --history -C --nocolor -q --quiet -l --locate -L --LOCATE -C --nocolor -q --quiet -s --short -u --update -C --nocolor -q --quiet -V --VERSION -C --nocolor -a --all -q --quiet -s --short -a --all -C --nocolor -I --INSTALLED -N --NOTINSTALLED -D --dev -n --names -q --quiet -r --reverse -s --short -x --ignorecase ' -- "$cur") )
+        else
+            COMPREPLY=(
+                $(apt-cache --no-generate pkgnames "$cur")
+                $(apt-cache dumpavail | command grep "^Source: $cur" | sort -u | cut -f2 -d" ")
+            )
+        fi
     fi
 }
 
